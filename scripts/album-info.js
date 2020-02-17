0f8060ee5c6a045c63fba877b4773ec8f0f354f7
zczerska <zofia.hello@gmail.com>
2020-02-17 15:24:46 -0500

    album-info changed

---
+++ scripts/album-info.js
@@ -0,0 +1,6 @@
+{
+	$('#album-title').text(album.title);
+	$('.artist').text(artist.title);
+	$('#release-info').text(release-info.title);
+	$('img#album-cover-art').attr('src', album.albumArtUrl);
+}
